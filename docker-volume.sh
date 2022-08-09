a8bcec451705f4f661fe0af615543d08cbe0944d
badrules <ahmad.badrul@vcgamers.com>
2022-08-09 19:13:57 +0700

    docker volume

---
+++ docker-volume.sh
@@ -0,0 +1,9 @@
+#volume seperti halnya seperti storage 
+#mengambil data dari luar ke dalam container
+docker volume ls
+
+# create volume
+❯ docker volume create --name contohvolume
+
+#r remove volume
+❯ docker volume rm contohvolume
