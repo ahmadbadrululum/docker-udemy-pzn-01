4a566e7e332d9306bbcecfb4f2d617c09d2a60f9
badrules <ahmad.badrul@vcgamers.com>
2022-08-15 08:21:28 +0700

    docker prune

---
+++ docker-prune.sh
@@ -0,0 +1,16 @@
+#docker prune menghapus semua container yg sudah distop
+❯ docker container container prune
+
+#docker image prune
+❯ docker image prune
+
+# contoh remove volume
+❯ docker volume prune
+WARNING! This will remove all local volumes not used by at least one container.
+Are you sure you want to continue? [y/N] y
+Deleted Volumes:
+f7e83b55c62b07613d1705aa96a66f75383f30cf3ad17a236590e05ca2be26f5
+mongorestore
+b6af331a89085d990104d7a76bb74f5a000b960227d0feb883c8a1bc5bb58766
+6e1aea387d22d06420d034d411671957f7003bfd5b63883f2d3714189602bfb3
+4bb7c83cf5954f6ab12d7893a727271d11f40fdcbb064c5d67692833e3cbdea2
